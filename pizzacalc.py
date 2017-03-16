02ac54befa1fe8fbb667f5cd4920c6ca6b676690
Nic Ricardson <Mr.NicRichardson@gmail.com>
2017-03-15 21:40:22 -0700

    added function clacTotalCost, which get subtotal, adds tip(percentage or amount), then adds tax

--- pizzacalc.py
+++ pizzacalc.py
@@ -1,32 +1,66 @@
 class Person(object):
 
-	moneyOwed = 0
+    moneyOwed = 0
 
-	def __init__(self):
-		self.name = self.getName()
+    def __init__(self):
+        self.name = self.getName()
 
-	def getName(self):
-		return input("Name?: ")
-		
-	def getcost(self):
-		while(True):
-			try:
-				cost = float(input("Cost?: "))
-				break
-			except ValueError:
-				print ("Please input a number: ")
-		return cost
+    def getName(self):
+        return input("Name?: ")
+        
+    def getcost(self):
+        while(True):
+            try:
+                cost = float(input("Cost?: "))
+                break
+            except ValueError:
+                print ("Please input a number: ")
+        return cost
 
-	def addPizza(self):
-		self.moneyOwed += self.getcost()
+    def addPizza(self):
+        self.moneyOwed += self.getcost()
         return self.moneyOwed
 
 
-print("Welcome to the Pizza Calculator:")
-OnePizzaPayer = input("Will one person be paying for the order? [Y/N]: ")
-if(OnePizzaPayer.tolower() == "y"):
-	#add code for making the pizza buyer not 
-else:
+def calcTotalCost(people, tax):
+    totalCost = 0
+    while(True):
+        try:
+            tipType = str(input("Tip percentage (T) or Tip amount (A)?: "))
+            if(tipType.lower() != "t" and tipType.lower() != "a"):
+                int("This line forces an error")
+            break
+        except ValueError:
+            print ("Please input valid option")
+
+    if(tipType.lower == "t"):
+        while(True):
+            try:
+                tip = float(input("What percentage do you want to tip?: "))
+                break
+            except ValueError:
+                print("Please input a number")
+    else: 
+        while(True):
+            try:
+                tip = float(input("What amount would you want to tip?: "))
+                break
+            except ValueError:
+                print("Please input a number: ")
+
+    if(tipType == "t"):
+        for person in people:
+            totalCost += person.moneyOwed
+        totalCost *= (1+(tip/100))
+
+    else:
+        for person in people:
+            totalCost += person.moneyOwed
+        totalCost += tip
+
+    totalCost *= (1+(tax/100))
+
+    return totalCost
 
 
-while(True):
+
